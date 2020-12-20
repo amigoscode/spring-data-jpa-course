f2e57576e10234e626e325ecca66211ffe902a0f
amigoscode <amigoscode@gmail.com>
2020-12-20 21:21:36 +0000

    section 3

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,6 +25,13 @@ public class Application {
                     21
             );
 
+            Student maria2 = new Student(
+                    "Maria",
+                    "Jones",
+                    "maria2.jones@amigoscode.edu",
+                    25
+            );
+
             Student ahmed = new Student(
                     "Ahmed",
                     "Ali",
@@ -31,33 +39,26 @@ public class Application {
                     18
             );
 
-            System.out.println("Adding maria and ahmed");
-            studentRepository.saveAll(List.of(maria, ahmed));
-
-            System.out.print("Number of students: ");
-            System.out.println(studentRepository.count());
+            studentRepository.saveAll(List.of(maria, ahmed, maria2));
 
             studentRepository
-                    .findById(2L)
+                    .findStudentByEmail("ahmed.ali@amigoscode.edu")
                     .ifPresentOrElse(
                             System.out::println,
-                            () -> System.out.println("Student with ID 2 not found"));
+                            () -> System.out.println("Student with email ahmed.ali@amigoscode.edu not found"));
 
-            studentRepository
-                    .findById(3L)
-                    .ifPresentOrElse(
-                            System.out::println,
-                            () -> System.out.println("Student with ID 3 not found"));
+            studentRepository.selectStudentWhereFirstNameAndAgeGreaterOrEqual(
+                    "Maria",
+                    21
+            ).forEach(System.out::println);
 
-            System.out.println("Select all students");
-            List<Student> students = studentRepository.findAll();
-            students.forEach(System.out::println);
+            studentRepository.selectStudentWhereFirstNameAndAgeGreaterOrEqualNative(
+                    "Maria",
+                    21
+            ).forEach(System.out::println);
 
-            System.out.println("Delete maria");
-            studentRepository.deleteById(1L);
-
-            System.out.print("Number of students: ");
-            System.out.println(studentRepository.count());
+            System.out.println("Deleting Maria 2");
+            System.out.println(studentRepository.deleteStudentById(3L));
         };
     }
 
--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -1,6 +1,31 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
+
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
+    List<Student> selectStudentWhereFirstNameAndAgeGreaterOrEqual(
+            String firstName, Integer age);
+
+    @Query(
+            value = "SELECT * FROM student WHERE first_name = :firstName AND age >= :age",
+            nativeQuery = true)
+    List<Student> selectStudentWhereFirstNameAndAgeGreaterOrEqualNative(
+            @Param("firstName") String firstName,
+            @Param("age") Integer age);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Student u WHERE u.id = ?1")
+    int deleteStudentById(Long id);
 }
