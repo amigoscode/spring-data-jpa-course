3b4c8f45d2c7e6b778ac082b575c1eb024e1452e
amigoscode <amigoscode@gmail.com>
2020-12-20 21:45:31 +0000

    section 3

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
