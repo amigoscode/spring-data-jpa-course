616d96e6f22e7f7230d326c7972cc14d5e711538
Bayu K <50491719+bayukembara@users.noreply.github.com>
2023-07-05 22:32:16 +0700

    Sequence

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,11 +1,21 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @Entity(name = "Student")
 public class Student {
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
